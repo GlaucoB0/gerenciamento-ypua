805ee61d236f3f53ebace4462d84a60a641ba04e
Pedro Lucas <izielucas2016@gmail.com>
2024-10-11 11:28:50 -0300

    created client interface

---
+++ back-end/src/interfaces/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Clientes {
+    nome: String;
+    // email: String;
+    // cep: String;
+    // numero: Number;
+    // endereco: String;
+    // bairro: String;
+    // cidade: String;
+    // estado: String;
+    // pais: String;
+    // telefone: String;
+    // cpf: String;
+}
