8a90ea55064b1164e7ee14f5a5e2195438c0f020
Pedro Lucas <izielucas2016@gmail.com>
2024-10-11 11:29:59 -0300

    created 'criarCliente' controller

--- back-end/src/controllers/controllerClientes.ts
+++ back-end/src/controllers/controllerClientes.ts
@@ -1,5 +1,25 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { createClientSchema } from "../helpers/zodValidations";
+import {Clientes} from '../interfaces/interfaces';
+import {createClientService} from '../services/clientesServices'
+import formatZodError from '../helpers/formatZodError';
 
 export const criarCliente = async (req: Request, res: Response) => {
-    
-}
+    try {
+        const bodyValidation = createClientSchema.safeParse(req.body);
+
+        if(!bodyValidation.success){
+            return res.status(400).json({message: "Os dados recebidos no corpo da aplicação são invalidos", detalhes: formatZodError(bodyValidation.error)});
+        }
+
+        const client:Clientes = bodyValidation.data;
+        const clienteCriado = await createClientService(client);
+        if(!clienteCriado){
+            return res.status(400).json({message: "Não foi possivel criar o Cliente!"});
+        }
+        res.status(201).end();
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: error})
+    }
+}   
