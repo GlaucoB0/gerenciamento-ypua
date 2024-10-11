b1d7f2cd890d892a09b0311536833b59524e30f5
Pedro Lucas <izielucas2016@gmail.com>
2024-10-11 11:31:43 -0300

    fixed interface

--- back-end/src/interfaces/interfaces.ts
+++ back-end/src/interfaces/interfaces.ts
@@ -1,13 +1,13 @@
 export interface Clientes {
     nome: String;
-    // email: String;
-    // cep: String;
-    // numero: Number;
-    // endereco: String;
-    // bairro: String;
-    // cidade: String;
-    // estado: String;
-    // pais: String;
-    // telefone: String;
-    // cpf: String;
+    email: String;
+    cep: String;
+    numero: Number;
+    endereco: String;
+    bairro: String;
+    cidade: String;
+    estado: String;
+    pais: String;
+    telefone: String;
+    cpf: String;
 }
