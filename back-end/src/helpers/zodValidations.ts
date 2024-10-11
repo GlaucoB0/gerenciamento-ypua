cf91d0a645b3851f5585b04d577c6e808c781953
Pedro Lucas <izielucas2016@gmail.com>
2024-10-11 09:18:25 -0300

    fixed

--- back-end/src/helpers/zodValidations.ts
+++ back-end/src/helpers/zodValidations.ts
@@ -5,5 +5,5 @@ const onlyLetter = /\p{L}/
 export const createClientSchema = z.object({
     nome: z.string({message: "O Nome deve ser uma string!"}).min(3, {message: "O Nome deve conter ao menos 3 caracteres!"}).max(255, {message: "O Nome não deve ultrapassar 255 caracteres"}).regex(onlyLetter),
     email: z.string({message: "O Email deve ser uma string!"}).email({message: "O Email deve ser no formato de email: nome@email.com"}).max(255, {message: "O Email deve conter no máximo 255 caracteres"}),
-    cep: z.string
+    cep: z.string()
 })
