e2e7295701389263056ef65c6a9634104bab59a3
Pedro Lucas <izielucas2016@gmail.com>
2024-10-11 09:08:01 -0300

    Rota de clientes começada!

--- back-end/src/routes/rotaClientes.ts
+++ back-end/src/routes/rotaClientes.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import routerCliente from "./rotaClientes";
 
 const router = Router();
 
-router.use('/clientes', routerCliente);
+router.get('/')
 
 export default router;
