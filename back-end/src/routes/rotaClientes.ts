43faccf1419b4815bc787de63815c201b8d441c3
Pedro Lucas <izielucas2016@gmail.com>
2024-10-14 08:05:22 -0300

    FIX/Created routes

--- back-end/src/routes/rotaClientes.ts
+++ back-end/src/routes/rotaClientes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { criarCliente } from "../controllers/controllerClientes";
+import { criarCliente, listarClientes, listarClientesPorNome } from "../controllers/controllerClientes";
 
 const router = Router();
 
-router.post('/', criarCliente)
+router.post('/criarClientes', criarCliente);
+router.get('/listarClientes', listarClientes);
+router.get('/porNome:nome', listarClientesPorNome)
 
 export default router;
