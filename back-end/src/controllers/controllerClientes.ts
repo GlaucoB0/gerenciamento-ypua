c6ef9e0329b8285c13d78de2164ee1ec3f719e91
Pedro Lucas <izielucas2016@gmail.com>
2024-10-14 07:58:07 -0300

    FIX/Get client return fixed

--- back-end/src/controllers/controllerClientes.ts
+++ back-end/src/controllers/controllerClientes.ts
@@ -32,6 +32,19 @@ export const listarClientes = async (req: Request, res: Response) => {
         if(getAllClients.length == 0){
             res.status(404).json({message: "Não foram encontrados clientes!"})
         }
+        res.status(200).json({data: getAllClients})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: error})
+    }
+}
+
+export const getClientByName = async (req: Request, res: Response) => {
+    try {
+        const clientsByName = await getAllClient();
+        if(clientsByName.length == 0){
+            res.status(404).json({message: "Não foram encontrados clientes!"})
+        }
         res.status(200).json({data: getAllClient})
     } catch (error) {
         console.error(error)
