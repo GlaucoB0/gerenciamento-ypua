508f06dc2f3599bb45654dea6f372446798edd66
Pedro <izielucas2016@gmail.com>
2024-10-16 20:36:44 -0300

    FEAT/Added admin validation to create employe and room

--- back-end/src/routes/rotaFuncionarios.ts
+++ back-end/src/routes/rotaFuncionarios.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { criarFuncionario, login } from "../controllers/controllerFuncionarios";
+import verifyAdmin from '../helpers/verifyAdmin'
 
 const router = Router();
 
-router.post('/criarFuncionarios', criarFuncionario);
+router.post('/criarFuncionarios', verifyAdmin ,criarFuncionario);
 router.post('/login', login)
 
 export default router;
--- back-end/src/routes/rotaQuartos.ts
+++ back-end/src/routes/rotaQuartos.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { criarQuartos, listarQuartos } from "../controllers/controllerQuartos";
+import verifyAdmin from '../helpers/verifyAdmin'
 
 const router = Router();
 
-router.post('/criarQuartos', criarQuartos);
+router.post('/criarQuartos', verifyAdmin ,criarQuartos);
 router.get('/listarQuartos', listarQuartos);
 
 export default router;
