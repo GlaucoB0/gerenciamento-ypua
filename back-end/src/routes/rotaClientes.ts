7d4330255820f37a901f1f1a8ffdbd9971f81f8a
Pedro <izielucas2016@gmail.com>
2024-10-16 20:35:24 -0300

    FEAT/Added token validation to create client

--- back-end/src/routes/rotaClientes.ts
+++ back-end/src/routes/rotaClientes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { criarCliente, listarClientes, listarClientesPorNome } from "../controllers/controllerClientes";
+import verifyAdmin from "../helpers/verifyAdmin";
 
 const router = Router();
 
-router.post('/criarClientes', criarCliente);
+router.post('/criarClientes', verifyAdmin, criarCliente);
 router.get('/listarClientes', listarClientes);
 router.get('/porNome/:nome', listarClientesPorNome)
 
