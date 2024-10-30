413c6b222aca9f1baf243bcc75fef0fc9510f717
lucarl07 <junitinho2007@gmail.com>
2024-10-30 11:16:00 -0300

    🔧 config: Make use of the modern Sass API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  }
 })
