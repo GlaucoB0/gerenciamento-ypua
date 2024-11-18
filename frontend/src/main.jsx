f078c88c0e4407522b04604a987049fb9872b77a
lucarl07 <junitinho2007@gmail.com>
2024-11-18 19:08:54 -0300

    🚧 Add routing to the project's main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
+// Dependências:
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { 
+  createBrowserRouter, RouterProvider 
+} from 'react-router-dom'
+
+// Folhas de Estilo:
 import './index.scss'
+
+// Componentes:
 import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
