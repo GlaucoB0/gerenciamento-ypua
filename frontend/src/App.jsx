4c3e246faa9ac94aa621d95ac92270d5d22abb0b
lucarl07 <junitinho2007@gmail.com>
2024-11-19 16:45:07 -0300

    ♻ refactor: Change routes using child routing and appViewLoader hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import { Navigate } from 'react-router-dom';
+import { redirect, Outlet } from 'react-router-dom';
 
 const App = () => {
-  const [isLogged, setLoginState] = useState(false)
-
   return (
     <>
-      { isLogged === true ? <Navigate to="/app" /> : <Navigate to="/login" /> }
+      <h1>Olaaaa</h1>
+      <Outlet />
     </>
   )
 }
