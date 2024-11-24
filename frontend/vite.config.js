f4d6a073d9d87d1f46e4385fa43768bdf0af4a18
lucarl07 <junitinho2007@gmail.com>
2024-11-24 18:10:20 -0300

    ➕ build: Add Axios & .env support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [react()],
   resolve: {
     alias: {
