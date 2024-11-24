dfc8c700de30bc4afa3f0ce4e671bd3ed0c01ddd
lucarl07 <junitinho2007@gmail.com>
2024-11-24 15:48:50 -0300

    👌 style: Remove relative imports from src/main.jsx

---
+++ frontend/src/hooks/actions/loginRequest.js
@@ -0,0 +1,10 @@
+
+
+const loginRequest = async ({ request, params }) => {
+  /** Aqui, o seguinte fluxo ocorrerá:
+   * - Validação dos dados enviados pelo Form
+   * - Envio dos dados para a API
+   * - Retorno com base nas respostas */
+}
+
+export default loginRequest;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-// Folhas de Estilo:
-import './Styles/main.scss'
+// Hooks:
+import { appViewLoader } from 'hooks/loaders/viewLoaders'
 
-// Componentes:
+// Visualizações & estilos:
+import 'styles/main.scss'
 import App from 'src/App'
 import AppView from 'views/AppView/AppView'
 import LoginView from 'views/LoginView/LoginView'
-import { appViewLoader } from 'src/hooks/loaders/viewLoaders'
 
 const router = createBrowserRouter([
   {
