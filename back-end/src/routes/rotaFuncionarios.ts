bc1cf261fee5ea3cc21ecfc551b0dde53d811ab6
Pedro Lucas <izielucas2016@gmail.com>
2024-11-25 09:24:43 -0300

    FIX/Fix route to get employe data by id

--- back-end/src/routes/rotaFuncionarios.ts
+++ back-end/src/routes/rotaFuncionarios.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { criarFuncionario, login } from "../controllers/controllerFuncionarios";
+import { criarFuncionario, login, listToken } from "../controllers/controllerFuncionarios";
 import verifyAdmin from '../helpers/verifyAdmin'
 
 const router = Router();
 
 router.post('/criarFuncionarios' ,criarFuncionario);
 router.post('/login', login)
+router.get('/listByToken', listToken);
 
 export default router;
