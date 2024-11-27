a3b7c59be890c146582a786a68d31148e5dc746b
lucarl07 <junitinho2007@gmail.com>
2024-11-26 23:41:00 -0300

    ✨ feat: Add HeadedCard w/ ReservasTitle to VisaoGeral

--- frontend/src/pages/VisaoGeral/VisaoGeral.jsx
+++ frontend/src/pages/VisaoGeral/VisaoGeral.jsx
@@ -1,15 +1,28 @@
-import $ from './VisaoGeral.module.sass'
+import $ from './VisaoGeral.module.scss'
+
+// Dependências:
+import { Link } from 'react-router-dom';
 
 // Componentes:
 import Card from 'components/Card/Card'
 import Text from 'components/Text/Text';
-import { Link } from 'react-router-dom';
-import Button from 'src/components/Button/Button';
+import HeadedCard from 'src/components/HeadedCard/HeadedCard';
 
-const VisaoGeral = () => {
+const ReservasTitle = () => {
+  return (
+    <Text type='Title2' color='white' fontFamily='black'>
+      Reservas
+    </Text>
+  );
+}
+
+export default function VisaoGeral() {
   return (
     <div className={$.page_grid}>
-      <Card orientation='horizontal' style={{ width: '30rem' }}>
+      <Card orientation='horizontal' style={{ 
+        width: '30rem',
+        height: '12.5rem'
+      }}>
         <section>
           <Text type='Title1' fontFamily='black'>
             Acomodações
@@ -28,7 +41,10 @@ const VisaoGeral = () => {
           <div className={$.block_s2} />
         </section>
       </Card>
-      <Card orientation='horizontal' style={{ width: '30rem' }}>
+      <Card orientation='horizontal' style={{ 
+        width: '30rem',
+        height: '12.5rem'
+      }}>
         <section className={$.text_wrapper}>
           <div>
             <Text type='Title1' fontFamily='black'>
@@ -48,11 +64,46 @@ const VisaoGeral = () => {
           <div className={$.block_s2} />
         </section>
       </Card>
-      <Card orientation='vertical' style={{ width: '25rem' }}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      </Card>
+      <HeadedCard 
+        cardStyle={{ width: '20rem', height: 'fit-content' }}
+        title={<ReservasTitle />}>
+        <ul className={$.reserves}>
+          <li className={$.reserves_by_type}>
+            <Text type='Title2'>Totais:</Text>
+            <div style={{ display: 'flex', gap: '1rem' }}>
+              <Text type='Title1'>100</Text>
+              <div className={$.block_mini_blue} />
+            </div>
+          </li>
+          <hr className={$.hr} />
+
+          <li className={$.reserves_by_type}>
+            <Text type='Title2'>Confirmadas:</Text>
+            <div style={{ display: 'flex', gap: '1rem' }}>
+              <Text type='Title1'>100</Text>
+              <div className={$.block_mini_green} />
+            </div>
+          </li>
+          <hr className={$.hr} />
+
+          <li className={$.reserves_by_type}>
+            <Text type='Title2'>Pendentes:</Text>
+            <div style={{ display: 'flex', gap: '1rem' }}>
+              <Text type='Title1'>100</Text>
+              <div className={$.block_mini_yellow} />
+            </div>
+          </li>
+          <hr className={$.hr} />
+
+          <li className={$.reserves_by_type}>
+            <Text type='Title2'>Canceladas:</Text>
+            <div style={{ display: 'flex', gap: '1rem' }}>
+              <Text type='Title1'>100</Text>
+              <div className={$.block_mini_red} />
+            </div>
+          </li>
+        </ul>
+      </HeadedCard>
     </div>
   )
 }
-
-export default VisaoGeral;
