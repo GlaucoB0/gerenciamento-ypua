4cd6ac336e2260a20a599f71a9c01bfb1e8ba876
lucarl07 <junitinho2007@gmail.com>
2024-11-27 23:11:32 -0300

    🗑 remove: 'setter' parameter at deleteReserva hook

--- frontend/src/hooks/tasks/deleteReserva.js
+++ frontend/src/hooks/tasks/deleteReserva.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
-const deleteReserva = async (reservaId, setter) => {
+const deleteReserva = async (reservaId) => {
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
   
   try {
     await axios.delete(`${baseUrl}/clientes/deleteById/${reservaId}`);
-    setter(true)
   } catch (error) {
     console.error(error);
     return;
