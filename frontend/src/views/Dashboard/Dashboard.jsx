eeec185b1f8e70651672863d3319649ddb1e9435
lucarl07 <junitinho2007@gmail.com>
2024-11-27 23:12:57 -0300

    ✨ feat: Add logout via button 'Sair'

--- frontend/src/views/Dashboard/Dashboard.jsx
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -1,17 +1,51 @@
 import $ from './Dashboard.module.sass'
-import { Outlet } from 'react-router-dom'
-import Card from 'src/components/Card/Card'
-import Navbar from 'src/components/Navbar'
 
+// Dependências:
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
+// Componentes:
+import Card from 'components/Card/Card'
+import Navbar from 'components/Navbar'
+import Button from 'components/Button/Button'
+import Modal from 'components/Modal/Modal'
+
+// Assets:
 import icon_VisaoGeral from 'assets/images/icon-filled-eye.png'
 import icon_Reservas from 'assets/images/icon-tag.png'
 import icon_ListaUsuarios from 'assets/images/icon-user-list.png'
 import icon_Criar from 'assets/images/icon-create.png'
 import icon_Sair from 'assets/images/icon-exit.png'
 
-const AppView = () => {
+const ExitModal = ({onCancel, onExit}) => {
+  return (
+    <Modal 
+      title="Sair da Conta"
+      text="Tem certeza de que deseja sair? O login deverá ser feito novamente.">
+      <Button className={$.custom_modal_btn} height="50px" onClick={onCancel}>
+        Cancelar
+      </Button>
+      <Button height="50px" onClick={onExit}>
+        Sair
+      </Button>
+    </Modal>
+  )
+}
+
+export default function Dashboard() {
+  const [show, setShow] = useState(false)
+  const navigate = useNavigate()
+  
+  const handleExit = () => {
+    localStorage.removeItem('user')
+    navigate('/login')
+  }
+
   return (
     <>
+      {show && (
+        <ExitModal onExit={handleExit} onCancel={() => setShow(false)} />
+      )}
       <aside className={$.aside}>
         <Card
           orientation='vertical'
@@ -19,18 +53,25 @@ const AppView = () => {
           style={{ height: '100%' }}>
           <Navbar.Root>
             <Navbar.Link imgSrc={icon_VisaoGeral} 
-              name="Visão Geral" to="/dashboard/geral" />
+              name="Visão Geral" to="/dashboard/geral" 
+            />
             <Navbar.Link imgSrc={icon_Reservas} 
-              name="Acomodações" to="/dashboard/acomodacoes" />
+              name="Acomodações" to="/dashboard/acomodacoes" 
+            />
             <Navbar.Link imgSrc={icon_ListaUsuarios} 
-              name="Lista de Hóspedes" to="/dashboard/hospedes" />
+              name="Lista de Hóspedes" to="/dashboard/hospedes" 
+            />
             <Navbar.Link imgSrc={icon_Criar} 
-              name="Criar Acomodação" to="/dashboard/criarAcomodacao" />
+              name="Criar Acomodação" to="/dashboard/criarAcomodacao" 
+            />
             <Navbar.Link imgSrc={icon_Criar} 
-              name="Criar Funcionário" to="/dashboard/criarFuncionario" />
+              name="Criar Funcionário" to="/dashboard/criarFuncionario" 
+            />
           </Navbar.Root>
-          {/* ⚠ AVISO: O elemento abaixo será substituído por um modal de logout. */}
-          <Navbar.Link imgSrc={icon_Sair} name="Sair" to="#" />
+          <Button className={$.custom_btn} onClick={() => setShow(true)}>
+            <img src={icon_Sair} alt="Sair da conta" />
+            Sair
+          </Button>
         </Card>
       </aside>
       <main className={$.main}>
@@ -39,5 +80,3 @@ const AppView = () => {
     </>
   )
 }
-
-export default AppView
