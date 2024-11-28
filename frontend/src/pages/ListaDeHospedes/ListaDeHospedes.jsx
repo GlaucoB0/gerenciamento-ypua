5621c2848ef60801105ad2a6416d262aaa18f207
lucarl07 <junitinho2007@gmail.com>
2024-11-27 23:12:04 -0300

    ♻ refactor: Adapt ListaDeHospedes to the updated Modal

--- frontend/src/pages/ListaDeHospedes/ListaDeHospedes.jsx
+++ frontend/src/pages/ListaDeHospedes/ListaDeHospedes.jsx
@@ -2,52 +2,73 @@
 import $ from "./ListaDeHospedes.module.sass";
 
 // Dependências e módulos:
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import deleteReserva from 'src/hooks/tasks/deleteReserva';
 
 // Componentes:
 import Hospede from "components/Hospede";
-import Model from "components/Model/Model";
+import Modal from "components/Modal/Modal";
 import Titulo from "components/Titulo/Titulo";
+import Button from "components/Button/Button";
 
-const ListaDeHospedes = () => {
+const DeletionModal = ({ onCancel, onDelete }) => {
+  return (
+    <Modal 
+      title="Remover reserva"
+      text="Deseja realmente remover essa reserva? Essa escolha é permanente.">
+      <Button 
+        height="50px"
+        className={$.custom_modal_btn} 
+        onClick={onCancel}>
+        Cancelar
+      </Button>
+      <Button height="50px" onClick={onDelete}>
+        Remover
+      </Button>
+    </Modal>
+  )
+}
+
+export default function ListaDeHospedes() {
   const lista = useLoaderData();
   const [model, setModel] = useState(false);
 
-  const handleDelete = (id, setter) => {
-    deleteReserva(id, setter)
+  const handleDelete = async (id) => {
+    await deleteReserva(id)
+    window.location.reload()
+    setModel(false)
   }
 
   return (
     <>
       <Titulo links={["Lista de Hospedes"]} />
-      <ul className={$.list}>
-        {lista.map((a, i) => (
-          <li key={i}>
-            <Hospede.Root>
-              <Hospede.Informacao
-                titulo={"Identificador"}
-                data={a.reserva_id}
-              />
-              <Hospede.Informacao titulo={"Nome"} data={a.nome} />
-              <Hospede.Informacao
-                titulo={"Acomodação"}
-                data={a.quarto.nome}
-              />
-              <Hospede.Informacao titulo={"Telefone"} data={a.telefone} />
-              <Hospede.Informacao
-                titulo={"Realizado em"}
-                data={a.data_reserva.split("T")[0]}
-              />
-              <Hospede.Button onClick={() => handleDelete(a.reserva_id, setModel)} />
-              {model && <Model text={'Tarefa Deletada!'} setModel={setModel}/>}
-            </Hospede.Root>
-          </li>
-        ))}
-      </ul>
+        <ul className={$.list}>
+          {lista.map((a, i) => (
+            <li key={i}>
+              <Hospede.Root>
+                <Hospede.Informacao
+                  titulo={"Identificador"}
+                  data={a.reserva_id}
+                />
+                <Hospede.Informacao titulo={"Nome"} data={a.nome} />
+                <Hospede.Informacao
+                  titulo={"Acomodação"}
+                  data={a.quarto.nome}
+                />
+                <Hospede.Informacao titulo={"Telefone"} data={a.telefone} />
+                <Hospede.Informacao
+                  titulo={"Realizado em"}
+                  data={a.data_reserva.split("T")[0]}
+                />
+                <Hospede.Button onClick={() => setModel(true)} />
+                {model && (
+                  <DeletionModal onCancel={() => setModel(false)} onDelete={() => handleDelete(a.reserva_id)} />
+                )}
+              </Hospede.Root>
+            </li>
+          ))}
+        </ul>
     </>
   );
 };
-
-export default ListaDeHospedes;
